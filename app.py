9500b6fc28cd4806bb2c79afabf80501aadb9810
Daniel Faria <danielfnfaria@gmail.com>
2021-03-26 17:01:49 -0300

    add /health

--- app.py
+++ app.py
@@ -3,9 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def main():
 	return 'Consegui :D!'
 
+@app.route('/health')
+def health():
+	return 'Tudo certo por aqui'
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=80)
